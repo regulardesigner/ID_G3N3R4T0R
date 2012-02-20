ee3be0fda228c5ca0759034427c2d59c89f46a21
Tailhades <damien.tailhades@gmail.com>
2012-02-20 17:13:58 +0100

    add function file and basic css

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<?php
+# footer de toutes les pages du site ;)
+?>
+	<!--[if IE 9]></div><![endif]-->
+	<!--[if IE 8]></div><![endif]-->
+	<!--[if IE 7]></div><![endif]-->
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<?php
+# header de toutes les pages
+?>
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/reset.css">
+	
+	<!-- [if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	
+	<title><?php echo $title; ?></title>
+	
+</head>
+
+<body>
+	<!--[if IE 7]><div id="ie7"><![endif]-->
+	<!--[if IE 8]><div id="ie8"><![endif]-->
+	<!--[if IE 9]><div id="ie9"><![endif]-->
